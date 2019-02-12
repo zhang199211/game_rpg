dab9863a56bc4d672dc82af96a9b8f57b3aa77c2
zhang199211 <853931397@qq.com>
2019-02-12 16:28:37 +0800

    first git

---
+++ Assets/Script/ActorController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorController : MonoBehaviour
+{
+    public GameObject model;
+    public PlayerInput pi;
+
+    public float moveSpeed;
+    public float runMultiplier=2;
+
+    [SerializeField]
+    private Animator anim;
+    private Rigidbody rigid;
+    private Vector3 movingVec;
+
+    private void Awake()
+    {
+        pi = gameObject.GetComponent<PlayerInput>();
+        anim = model.GetComponent<Animator>();
+
+        rigid = gameObject.GetComponent<Rigidbody>();
+
+
+    }
+
+    private void Update()
+    {
+        float targetRunMulti = (pi.run ? 2.0f : 1.0f);
+        anim.SetFloat("forward", pi.Dmag * Mathf.Lerp(anim.GetFloat("forward"), targetRunMulti, 0.1f));
+        if (pi.jump)
+        {
+            anim.SetTrigger("jump");
+        }
+        if (pi.Dmag > 0.1f)
+        {
+            Vector3 targetForward = Vector3.Slerp(model.transform.forward, pi.DVec,0.3f);
+            model.transform.forward = targetForward;
+        }
+        movingVec = pi.Dmag * model.transform.forward* moveSpeed*(pi.run? runMultiplier : 1.0f);
+    }
+
+    private void FixedUpdate()
+    {
+        rigid.position += movingVec * Time.fixedDeltaTime;
+    }
+}
---
+++ Assets/Script/PlayerInput.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    [Header("====  Key setting  ====")]
+    //public string keyUp="w";
+    //public string keyDown="s";
+    //public string keyLeft="a";
+    //public string keyRight="d";
+    private KeyCode keyUp=KeyCode.W;
+    private KeyCode keyDown = KeyCode.S;
+    private KeyCode keyLeft = KeyCode.A;
+    private KeyCode keyRight = KeyCode.D;
+    private KeyCode keyA = KeyCode.LeftShift;
+    private KeyCode keyB = KeyCode.J ;
+    private KeyCode keyC = KeyCode.A;
+    private KeyCode keyD = KeyCode.D;
+
+    [Header("====  Output signals  ====")]
+    public float Dup;
+    public float Dright;
+    [Header("向量长度")]
+    public float Dmag;
+    //模型向前向量
+    public Vector3 DVec;
+
+    // 1.pressing signals
+    public bool run;
+    // 2.trigger once signals;
+    public bool jump;
+    private bool lastJump;
+    // 3.double trigger
+
+
+    [Header("====  Others  ====")]
+    public bool inputEnabled = true;
+    private float targetDup;
+    private float targetDright;
+    private float velocityDup;
+    private float velocityDright;
+     
+    private void Update()
+    {
+        targetDup = (Input.GetKey(keyUp) ? 1.0f : 0f)-(Input.GetKey(keyDown)?1.0f:0f);
+        targetDright = (Input.GetKey(keyRight) ? 1.0f : 0f) - (Input.GetKey(keyLeft) ? 1.0f : 0f);
+        if (!inputEnabled)
+        {
+            targetDup = 0;
+            targetDright = 0;
+        }
+        Dup = Mathf.SmoothDamp(Dup,targetDup,ref velocityDup,0.1f);
+        Dright = Mathf.SmoothDamp(Dright, targetDright, ref velocityDright, 0.1f);
+
+        Vector2 tempDAxis = SquareToCirle(new Vector2(Dup, Dright));
+        float Dup2 = tempDAxis.y;
+        float Dright2 = tempDAxis.x;
+
+
+        Dmag = Mathf.Sqrt((Dup2 * Dup2) + (Dright2 * Dright2));
+        DVec = Dright2 * transform.right + Dup2 * transform.forward;
+
+        run = Input.GetKey(keyA);
+
+        bool newJump=Input.GetKey(keyB);
+        if (newJump !=lastJump&& newJump == true)
+        {
+            jump = true;
+        }
+        else
+        {
+            jump = false;
+        }
+        lastJump = newJump;
+
+    }
+
+    private Vector2 SquareToCirle(Vector2 input)
+    {
+        Vector2 output = Vector2.zero;
+        output.x = input.x * Mathf.Sqrt(1 - (input.y * input.y) / 2.0f);
+        output.y = input.y * Mathf.Sqrt(1 - (input.x * input.x) / 2.0f);
+        return output;
+    }
+
+}
